c3505e6084f1238a147582f3528cdcd3e61f4c05
Admin <beshovetch@gmail.com>
2024-08-02 14:20:13 +0400

    some_edit

--- lib/profile_tab.dart
+++ lib/profile_tab.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ProfileTab extends StatelessWidget {
-   ProfileTab({super.key});
+  ProfileTab({super.key});
 
   var nameController = TextEditingController();
   var phoneController = TextEditingController();
@@ -11,6 +11,7 @@ class ProfileTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: Colors.black12,
       appBar: AppBar(
         backgroundColor: Colors.grey[800],
@@ -18,96 +19,132 @@ class ProfileTab extends StatelessWidget {
         title: const Text(
           "Profile",
           style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              color: Colors.white
-          ),
+              fontSize: 20, fontWeight: FontWeight.w400, color: Colors.white),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Name",
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.white),
-            ),
-            TextFormField(
-              controller: nameController,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.edit,color: Colors.white,),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        border: Border.all(color: Colors.white),),
+                    child: const Icon(Icons.person,color: Colors.grey,size: 200,)
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "Phone Number",
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.white),
-            ),
-            TextFormField(
-              controller: phoneController,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.phone,color: Colors.white,),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+              const Text(
+                "Name",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.white),
+              ),
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.text,
+                controller: nameController,
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "Email",
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.white),
-            ),
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.email,color: Colors.white,),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Phone Number",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.white),
+              ),
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.phone,
+                controller: phoneController,
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "Password",
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.white),
-            ),
-            TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.password,color: Colors.white,),
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(style: BorderStyle.solid,width: 5,color: Colors.white),
-                  borderRadius: BorderRadius.circular(10),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Email",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.white),
+              ),
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.text,
+                controller: emailController,
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Password",
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.white),
+              ),
+              TextFormField(
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  suffixIcon: const Icon(
+                    Icons.password,
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        style: BorderStyle.solid,
+                        width: 5,
+                        color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
